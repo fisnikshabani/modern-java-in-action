c88dc5f81efd7e7149e23b8649200032b5a8ee86
Fisnik Shabani <fisnik.shabani@gmail.com>
2025-03-16 02:55:24 +0100

    Expand chapter5 examples

--- src/main/java/org/example/chapter5/PuttingIntoPractise.java
+++ src/main/java/org/example/chapter5/PuttingIntoPractise.java
@@ -1,9 +1,6 @@
 package org.example.chapter5;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PuttingIntoPractise {
@@ -54,9 +51,45 @@ public class PuttingIntoPractise {
         System.out.println(tradersFromCambridge);
 
         //Returns a string of all traders’ names sorted alphabetically
+        String traderNames = transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .distinct()
+                .sorted()
+                .reduce("", (n1,n2) -> n1 + n2);
+        System.out.println(traderNames);
 
+        String traderNames2 = transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .distinct()
+                .sorted()
+                .collect(Collectors.joining());
+        System.out.println(traderNames2);
 
+        //are any traders based in Milan?
+        boolean milanBased = transactions.stream()
+                .anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
+        System.out.println(milanBased);
 
+        //print all transaction's values from the traders living in Cambridge
+        transactions.stream()
+                .filter(transaction -> "Cambridge".equals(transaction.getTrader().getCity()))
+                .map(Transaction::getValue)
+                .forEach(System.out::println);
 
+        //what's the highest value of all transactions
+        Optional<Integer> highestValue = transactions.stream()
+                .map(Transaction::getValue)
+                .reduce(Integer::max);
+        System.out.println("The highest value of all transactions is: " + highestValue.get());
+
+        //find the transaction with the smallest value
+        Optional<Transaction> smallestTransaction = transactions.stream()
+                .reduce((t1,t2) -> t1.getValue() < t2.getValue() ? t1 : t2);
+        System.out.println("The smallest transaction is: " + smallestTransaction.get());
+
+        //other way
+        Optional<Transaction> smallestTransaction2 = transactions.stream()
+                .min(Comparator.comparing(Transaction::getValue));
+        System.out.println("The smallest transaction is: " + smallestTransaction2.get());
     }
 }
